3f7715dd0e28cc8139fa8513268204216280a283
prabhu <prabhuyadav748@gmail.com>
2026-05-30 22:38:09 +0530

    update

---
+++ kids_story_generator.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Wrapper for backwards compatibility: some workflows call kids_story_generator.py
+from generate_kids_story import main
+
+if __name__ == '__main__':
+    main()
